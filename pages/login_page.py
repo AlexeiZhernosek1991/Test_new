478dd262dfc1d713e0c55e34bfedfc292ee34a13
AlexeiZhernosek1991 <115401266+AlexeiZhernosek1991@users.noreply.github.com>
2025-05-26 20:49:54 +0300

    test remove

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,17 +1,16 @@
 from pages.page_model import BasePage
 
+
 class LoginPage(BasePage):
     PAGE_URL = "https://www.saucedemo.com"
     LOGIN_FILD = "Username"
     PASSWORD_FILD = 'Password'
 
-    def enter_login (self, login):
+    def enter_login(self, login):
         self.page.get_by_placeholder(self.LOGIN_FILD).fill(login)
 
-
-    def enter_password (self, password):
+    def enter_password(self, password):
         self.page.get_by_placeholder(self.PASSWORD_FILD).fill(password)
 
-    def enter_button_login (self):
+    def enter_button_login(self):
         self.page.get_by_role("button").click()
-
